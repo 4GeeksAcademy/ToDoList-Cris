2f500825b9365621fdc3c6e0e4e6b059ae8e6866
Cristian Trapiello <119610379+CrisMader@users.noreply.github.com>
2025-11-26 11:54:11 +0000

    ToDoList Estilizado

--- src/js/components/ToDoList.jsx
+++ src/js/components/ToDoList.jsx
@@ -2,51 +2,57 @@ import React, { useState } from "react";
 
 const ToDoList = () => {
     const [doList, setDoList] = useState("");
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        if(doList === "") return alert("Debes escribir algo para incluirlo en la lista")
-        setData([...data, doList])
-        setDoList("")
-    }
+        if (doList === "") return alert("Debes escribir algo que añadir a la lista");
+        setData([...data, doList]);
+        setDoList("");
+    };
+
+    const handleDelete = (i) => {
+        setData(data.filter((element, index) => index !== i));
+    };
 
     const handleChange = (e) => {
         setDoList(e.target.value);
     }
 
-    const handleDelete = (i) =>{
-        setData(data.filter((_, index) => index !== i));
-    }
-
-    console.log(data);
+    console.log(data)
 
     return (
-        <>
+        <div className="paper">
             <form onSubmit={handleSubmit}>
                 <input
-                    onChange={handleChange}
                     type="text"
                     value={doList}
-                    name="todolist"
-                    placeholder="¿Que tenemos que hacer hoy...?"
+                    onChange={handleChange}
+                    placeholder="¿Qué tenemos que hacer hoy...?"
                 />
-                <input type="submit" />
             </form>
 
-            {
-                <ul>
-                    {data.map((task, i) =>
-                        <li key={i}>
+            <ul>
+                {data.map((task, i) => (
+                    <div key={i}>
+                        <li>
                             {task}
-                    <button onClick={() => handleDelete(i)}>
-                        ❌
-                    </button>
-                        </li>)}
-                </ul>
-            }
-        </>
+                            <button
+                                className="btn btn-danger"
+                                onClick={() => handleDelete(i)}>
+                                X
+                            </button>
+                        </li>
+                        <hr className="line" />
+                    </div>
+                ))}
+            </ul>
+
+            {data.length === 0 && (
+                <p className="empty-message">Sin tareas pendientes</p>
+            )}
+        </div>
     );
-}
+};
 
 export default ToDoList;
